4426de0975f0c1b9b6a3234edd673b9af838bfcb
KunjTibrewal <kunjtib2@gmail.com>
2026-05-15 15:41:17 +0530

    understanding variable scope and built in data types...
    today i struggled with global and local variables, trying to use both with same name... later solved this by experimenting as seen in code...

---
+++ understanding_variable_scope_and_built-in_data.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//global variable
+int GlobalVariable = 10;
+void sum(){
+    // int GlobalVariable;
+    //figured out that id you declare a variable but dont give value to it, it takes random value
+    cout<< "\nThis is a global variable: "<< GlobalVariable;
+    /*i still dont know about functions like sum void*/
+}
+int main(){
+    int one = 1, two = 2;
+    float pi = 3.14;
+    char letter = 'A';
+    double pi_upto_10_decimal_places = 3.1415926535;
+    bool is_true = true;
+     sum(); // Calling the function to demonstrate global variable access
+     /*according to my understanding, before defining local variable you need to demonstrate
+     global variable access else, it will take local variable defined in this function*/
+    GlobalVariable = 20; // Modifying the global variable
+    cout<< "\nOne: "<< one;
+    cout<< "\nTwo: "<< two;
+    cout<< "\nPi: "<< pi;
+    cout<< "\nFirst alphabet: "<< letter;
+    cout<< "\nPi up to 10 decimal places: "<< pi_upto_10_decimal_places << " Is it true? "<< is_true;
+    // sum(); // Calling the function to demonstrate global variable access
+    cout<< "\nNow using local variable with same name as global variable: "<< GlobalVariable;
+    return 0;
+}
