d5d1212b2d993ec63160600b319eead7bb62edf6
KunjTibrewal <kunjtib2@gmail.com>
2026-05-16 23:49:06 +0530

    learned how to use global variable when local is defined

--- understanding_variable_scope_and_built-in_data.cpp
+++ understanding_variable_scope_and_built-in_data.cpp
@@ -20,8 +20,10 @@ int main(){
     cout<< "\nTwo: "<< two;
     cout<< "\nPi: "<< pi;
     cout<< "\nFirst alphabet: "<< letter;
-    cout<< "\nPi up to 10 decimal places: "<< pi_upto_10_decimal_places << " Is it true? "<< is_true;
+    cout<< "\nPi up to 10 decimal places: "<< pi_upto_10_decimal_places << " Is it true? "<< is_true << endl;
     sum(); // Calling the function to demonstrate global variable access
     cout<< "\nNow using local variable with same name as global variable: "<< GlobalVariable;
+    /* 16-05-2026, learned how to use global variables when local variables are declared with the same name */
+    cout << "\nTo access the global variable when a local variable with the same name exists: " << ::GlobalVariable;
     return 0;
 }
