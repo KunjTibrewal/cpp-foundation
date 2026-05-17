7dc538e81467a5269afa01b6f250b6fee393e43d
KunjTibrewal <kunjtib2@gmail.com>
2026-05-17 16:15:04 +0530

    fixed an old program of greatest integer function
    Fixed negative number bug in GIF program using if-else. Discovered another edge case for numbers like -5, -6 etc attempted switch-case but found it doesn't work for floats, resolved using nested if-else instead.

--- greatest_integer_function.cpp
+++ greatest_integer_function.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 int main(){
     float a;
-    cout << "Enter a positive number 'a': "; /*this program aint working for negative numbers
+    int b;
+    cout << "Enter a number 'a': "; /*this program aint working for negative numbers
     as of now... will try to fix it after if else statements are covered*/
     cin >> a;
-    int b = int(a); //greatest integer function
-    float c = a - int(a) ; //fractional part of a
+    if(a<0){ //17-05-2026 fixed negative number GIF problem
+        b = int(a) - 1; //greatest integer function
+    } else{
+        b = int(a);
+    }
+    float c = a - b ; //fractional part of a
+    if(c==1) { b = b + 1, c = 0; }
+    else{}
+        // switch(c){
+    //    case 1:      Tried using switch but it only works for integers and not for floats
+     //   b = b + 1;
+     //   break;
+     
     cout << "The greatest integer function of a is: " << b << endl;
     cout << "The fractional part of a is: " << c << endl;
     return 0;
