c3fca6a1d13a4dc9e94bf520f12c3cb7a00b1103
KunjTibrewal <kunjtib2@gmail.com>
2026-05-18 17:04:47 +0530

    made a factors finder program using for loop

---
+++ factors_finder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "Factors of " << number << " are: " <<endl;
+    for (int i = 1; i <= number; i++){
+        if (number % i == 0) {
+            cout << i << endl;
+         } else { }
+
+        }
+        return 0;   
+    }
