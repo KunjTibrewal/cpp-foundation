ea6974a2d21e720cf92aedd1df91692167d4a0d1
KunjTibrewal <kunjtib2@gmail.com>
2026-06-02 01:16:05 +0530

    Update understanding_classes_public-and-private-object-modifiers.cpp

--- understanding_classes_public-and-private-object-modifiers.cpp
+++ understanding_classes_public-and-private-object-modifiers.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 class employee{
-    private:
-        int sallary;
-        string password;
+    private:  // can not be accessed other than inside the class employee
+        int salary;
+        string password;        
     public: 
         int id;
         string name;
@@ -13,13 +13,11 @@ class employee{
 };
 
 void employee::setDetails(int s, string p){
-    sallary = s;
+    salary = s;
     password = p;
 }
 void employee::showDetails(){
-    cout << "The id of the employee is: " << id << endl;
-    cout << "The name of the employee is: " << name << endl;
-    cout << "The sallary of the employee is: " << sallary << endl;
+    cout << "The salary of the employee is: " << salary << endl;
     cout << "The password of the employee is: " << password << endl;
 }
 int main(){
@@ -28,17 +26,20 @@ int main(){
     cin >> e1.id;
     cout << "Enter the name of the employee: ";
     cin >> e1.name;
+    // cout << "Enter the password of the employee: ";
+    // cin >> e1.password;     will show error because password is a private member of the class employee
     int s;
     string p;
-    cout << "Enter the sallary of the employee: ";
+    cout << "Enter the salary of the employee: ";
     cin >> s;
     cout << "Enter the password of the employee: ";
     cin >> p;
     e1.setDetails(s, p);
+    s= 0, p = "";
     cout << "The id of the employee is: " << e1.id << endl;
     cout << "The name of the employee is: " << e1.name << endl;
-    // cout << "The sallary of the employee is: " << e1.sallary << endl;
-    // cout << "The password of the employee is: " << e1.password << endl;
+    // cout << "The salary of the employee is: " << e1.salary << endl;
+    // cout << "The password of the employee is: " << e1.password << endl;    will show error because salary and password are private members of the class employee
     e1.showDetails();
     return 0;
 }
